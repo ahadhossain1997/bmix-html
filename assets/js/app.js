c603d681844aab6f3462735aac64749a9c9d273e
ahad hossain <wpahad1997@gmail.com>
2024-08-02 22:55:10 +0600

    bmix start

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,22 +5,22 @@
 
   /*------------------------------------------------------------------
   [Table of contents]
-  medicx PRELOADER JS INIT
-  medicx STICKY MENU JS INIT
-  medicx HERO SLIDER JS INIT
-  medicx COUNTER JS INIT
-  medicx TESTIMONIAL SLIDER JS INIT
-  medicx SERVICE SLIDER SECTION JS INIT
-  medicx SLIDER LOGO JS INIT
-  medicx SERVICE CONTENT SLIDER SECTION JS INIT
-  medicx MAGNIFIC POPUP JS INIT
-  medicx TESTIMONIAL SLIDER JS INIT
-  medicx PORTFOLIO MASONAY FILTER JS
-  medicx MAP JS INIT
-  medicx TOGGOLE PASSWOR JS INIT
-  medicx WOW JS INIT
-  medicx DATEPICKER JS INIT
-  medicx SELECT OPTION JS INIT
+  bmix PRELOADER JS INIT
+  bmix STICKY MENU JS INIT
+  bmix HERO SLIDER JS INIT
+  bmix COUNTER JS INIT
+  bmix TESTIMONIAL SLIDER JS INIT
+  bmix SERVICE SLIDER SECTION JS INIT
+  bmix SLIDER LOGO JS INIT
+  bmix SERVICE CONTENT SLIDER SECTION JS INIT
+  bmix MAGNIFIC POPUP JS INIT
+  bmix TESTIMONIAL SLIDER JS INIT
+  bmix PORTFOLIO MASONAY FILTER JS
+  bmix MAP JS INIT
+  bmix TOGGOLE PASSWOR JS INIT
+  bmix WOW JS INIT
+  bmix DATEPICKER JS INIT
+  bmix SELECT OPTION JS INIT
   -------------------------------------------------------------------*/
 
   /*--------------------------------------------------------------
@@ -32,13 +32,13 @@
   };
   $(function () {
     /*--------------------------------------------------------------
-    medicx PRELOADER JS INIT
+    bmix PRELOADER JS INIT
     --------------------------------------------------------------*/
 
-    $(".medicx-preloader-wrap").fadeOut(500);
+    $(".bmix-preloader-wrap").fadeOut(500);
 
     /*--------------------------------------------------------------
-    medicx STICKY MENU JS INIT
+    bmix STICKY MENU JS INIT
     --------------------------------------------------------------*/
     $(window).on('scroll', function () {
       if ($(window).scrollTop() > 50) {
@@ -49,233 +49,25 @@
     });
 
     /*--------------------------------------------------------------
-    medicx HERO SLIDER JS INIT
+    bmix HERO SLIDER JS INIT
     --------------------------------------------------------------*/
 
-    /*----------- Custom Animaiton For Slider ----------*/
-    $('[data-ani-duration]').each(function () {
-      var durationTime = $(this).data('ani-duration');
-      $(this).css('animation-duration', durationTime);
-    });
-    $('[data-ani-delay]').each(function () {
-      var delayTime = $(this).data('ani-delay');
-      $(this).css('animation-delay', delayTime);
-    });
-    $('[data-ani]').each(function () {
-      var animaionName = $(this).data('ani');
-      $(this).addClass(animaionName);
-      $('.slick-current [data-ani]').addClass('slider-animated');
-    });
-    $('.global-carousel').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-      $(slick.$slides).find('[data-ani]').removeClass('slider-animated');
-      $(slick.$slides[currentSlide]).find('[data-ani]').addClass('slider-animated');
-    });
-    var hero_slider = $('.medicx-hero-slider-init');
-    if (hero_slider.is_exist()) {
-      hero_slider.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: true,
-        infinite: true,
-        speed: 1000,
-        lazyLoad: 'progressive',
-        prevArrow: '<button class="slide-arrow medicx-hero-next"></button>',
-        nextArrow: '<button class="slide-arrow medicx-hero-prev"></button>'
-      }).slickAnimation();
-    }
-    var hero_slider = $('.medicx-hero-slider-init2');
-    if (hero_slider.is_exist()) {
-      hero_slider.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: false,
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        lazyLoad: 'progressive'
-      }).slickAnimation();
-    }
-    var hero_slider = $('.medicx-hero-slider-init3');
-    if (hero_slider.is_exist()) {
-      hero_slider.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: false,
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        lazyLoad: 'progressive'
-      }).slickAnimation();
-    }
     /*--------------------------------------------------------------
-    medicx COUNTER JS INIT
-    --------------------------------------------------------------*/
-    var medicx_counter = $('#medicx-counter');
-    if (medicx_counter.is_exist()) {
-      var a = 0;
-      $(window).scroll(function () {
-        var oTop = $(medicx_counter).offset().top - window.innerHeight;
-        if (a == 0 && $(window).scrollTop() > oTop) {
-          $('.medicx-counter').each(function () {
-            var $this = $(this),
-              countTo = $this.attr('data-percentage');
-            $({
-              countNum: $this.text()
-            }).animate({
-              countNum: countTo
-            }, {
-              duration: 4000,
-              easing: 'swing',
-              step: function step() {
-                $this.text(Math.floor(this.countNum));
-              },
-              complete: function complete() {
-                $this.text(this.countNum);
-              }
-            });
-          });
-          a = 1;
-        }
-      });
-    }
-
-    /*--------------------------------------------------------------
-    medicx TESTIMONIAL SLIDER JS INIT
-    --------------------------------------------------------------*/
-    var t_slider = $('.medicx-testimonial-slider-init');
-    if (t_slider.is_exist()) {
-      t_slider.slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: false,
-        infinite: true,
-        centerMode: true,
-        centerPadding: '200px',
-        lazyLoad: 'progressive',
-        prevArrow: '<button class="slide-arrow medicx-t-next"></button>',
-        nextArrow: '<button class="slide-arrow medicx-t-prev"></button>',
-        responsive: [{
-          breakpoint: 1349,
-          settings: {
-            slidesToShow: 2
-          }
-        }, {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 1
-          }
-        }, {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            centerPadding: '100px'
-          }
-        }, {
-          breakpoint: 575,
-          settings: {
-            slidesToShow: 1,
-            centerPadding: '0px'
-          }
-        }]
-      });
-    }
-    var t_slider = $('.medicx-testimonialv2-slider-init');
-    if (t_slider.is_exist()) {
-      t_slider.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: true,
-        infinite: true,
-        lazyLoad: 'progressive',
-        prevArrow: '<button class="slide-arrow medicx-t-next"></button>',
-        nextArrow: '<button class="slide-arrow medicx-t-prev"></button>'
-      }).slickAnimation();
-    }
-    var t_slider = $('.medicx-testimonial-slider-init3');
-    if (t_slider.is_exist()) {
-      t_slider.slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: false,
-        dots: true,
-        infinite: true,
-        lazyLoad: 'progressive',
-        responsive: [{
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 1
-          }
-        }]
-      });
-    }
-
-    // medicx SERVICE SLIDER SECTION JS INIT
-    var slider_service_section = $('.medicx-slider-service-section');
-    if (slider_service_section.is_exist()) {
-      slider_service_section.slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 8000,
-        arrows: false,
-        pauseOnHover: false,
-        cssEase: 'linear'
-      });
-    }
-
-    /*--------------------------------------------------------------
-    medicx SLIDER LOGO JS INIT
+    bmix COUNTER JS INIT
     --------------------------------------------------------------*/
 
-    var slider_logo = $('.medicx-slider-logo-wrap');
-    if (slider_logo.is_exist()) {
-      slider_logo.slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 4000,
-        arrows: false,
-        pauseOnHover: false,
-        cssEase: 'linear',
-        responsive: [{
-          breakpoint: 1399,
-          settings: {
-            slidesToShow: 5
-          }
-        }, {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 3
-          }
-        }, {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 3
-          }
-        }, {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 2
-          }
-        }, {
-          breakpoint: 500,
-          settings: {
-            slidesToShow: 1
-          }
-        }]
-      });
-    }
+    /*--------------------------------------------------------------
+    bmix TESTIMONIAL SLIDER JS INIT
+    --------------------------------------------------------------*/
+
+    // bmix SERVICE SLIDER SECTION JS INIT
 
     /*--------------------------------------------------------------
-    medicx MAGNIFIC POPUP JS INIT
+    bmix SLIDER LOGO JS INIT
+    --------------------------------------------------------------*/
+
+    /*--------------------------------------------------------------
+    bmix MAGNIFIC POPUP JS INIT
     ------------------------------------------------------------*/
     var popup_youtube = $('.video-init');
     if (popup_youtube.is_exist()) {
@@ -290,77 +82,8 @@
 
   $(window).on("load", function () {
     /*--------------------------------------------------------------
-    medicx PORTFOLIO MASONAY FILTER JS
+    bmix PORTFOLIO MASONAY FILTER JS
     ------------------------------------------------------------*/
-
-    var medicx_filter_gallery = $('#medicx-portfolio-grid');
-    if (medicx_filter_gallery.is_exist()) {
-      var $container = $(medicx_filter_gallery),
-        colWidth = function colWidth() {
-          var w = $container.width(),
-            columnNum = 1,
-            columnWidth = 0;
-          if (w > 1200) {
-            columnNum = 2;
-          } else if (w > 900) {
-            columnNum = 2;
-          } else if (w > 600) {
-            columnNum = 2;
-          } else if (w > 450) {
-            columnNum = 1;
-          } else if (w > 385) {
-            columnNum = 1;
-          }
-          columnWidth = Math.floor(w / columnNum);
-          $container.find('.collection-grid-item').each(function () {
-            var $item = $(this),
-              multiplier_w = $item.attr('class').match(/collection-grid-item-w(\d)/),
-              multiplier_h = $item.attr('class').match(/collection-grid-item-h(\d)/),
-              width = multiplier_w ? columnWidth * multiplier_w[1] : columnWidth,
-              height = multiplier_h ? columnWidth * multiplier_h[1] * 0.4 - 12 : columnWidth * 0.5;
-            $item.css({
-              width: width
-            });
-          });
-          return columnWidth;
-        },
-        isotope = function isotope() {
-          $container.isotope({
-            resizable: false,
-            itemSelector: '.collection-grid-item',
-            masonry: {
-              columnWidth: colWidth(),
-              gutterWidth: 0
-            }
-          });
-        };
-      isotope();
-      $(window).resize(isotope);
-      var $optionSets = $('.medicx-portfolio-menu .option-set'),
-        $optionLinks = $optionSets.find('li');
-      $optionLinks.click(function () {
-        var $this = $(this);
-        var $optionSet = $this.parents('.option-set');
-        $optionSet.find('.active').removeClass('active');
-        $this.addClass('active');
-
-        // make option object dynamically, i.e. { filter: '.my-filter-class' }
-        var options = {},
-          key = $optionSet.attr('data-option-key'),
-          value = $this.attr('data-option-value');
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[key] = value;
-        if (key === 'layoutMode' && typeof changeLayoutMode === 'function') {
-          // changes in layout modes need extra logic
-          changeLayoutMode($this, options);
-        } else {
-          // creativewise, apply new options
-          $container.isotope(options);
-        }
-        return false;
-      });
-    }
   }); // End window LODE
 
   $(window).on("resize", function () {}); // end window resize
@@ -368,7 +91,7 @@
   $(window).on("load", function () {}); // End window LODE
 
   /*--------------------------------------------------------------
-  medicx MAP JS INIT
+  bmix MAP JS INIT
   ------------------------------------------------------------*/
   var google_map = $('#map');
   if (google_map.is_exist()) {
@@ -501,7 +224,7 @@
 
   // external js: isotope.pkgd.js
 
-  // medicx TOGGOLE PASSWOR JS INIT
+  // bmix TOGGOLE PASSWOR JS INIT
 
   $(".toggle-password").click(function () {
     $(this).toggleClass("fa-eye fa-eye-slash");
@@ -513,7 +236,7 @@
     }
   });
 
-  // medicx WOW JS INIT
+  // bmix WOW JS INIT
 
   var wow = new WOW({
     mobile: false,
@@ -527,7 +250,7 @@
   var currentYear = new Date().getFullYear();
   $('#current-year').text(currentYear);
 
-  // medicx DATEPICKER JS INIT
+  // bmix DATEPICKER JS INIT
 
   jQuery(document).ready(function () {
     jQuery('#datepicker').datepicker({
@@ -544,7 +267,7 @@
     });
   });
 
-  // medicx SELECT OPTION JS INIT
+  // bmix SELECT OPTION JS INIT
 
   var index = 1;
   var on = function on(listener, query, fn) {
